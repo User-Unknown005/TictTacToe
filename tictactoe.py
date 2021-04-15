9a6dca5cff256064569798ada88faffa8fdf6323
Shourya Deep Bera <67041876+User-Unknown005@users.noreply.github.com>
2021-04-15 13:12:23 +0530

    Create tictactoe.py

---
+++ tictactoe.py
@@ -0,0 +1,16 @@
+import os
+print(".,,.....Main Menu.....,,.")
+print("1. Start 2 players game")
+print("2. Contact the developer")
+print("3. Update")
+print("4. Quit")
+
+choice=int(input("Enter your choice"))
+if choice == 1:
+ os.system("python3 main.py")
+elif choice == 2:
+ os.system("adb shell am start -a android.intent.action.VIEW -d http://https://bit.ly/3e7SfnR")
+elif choice == 3:
+ os.system("bash update.sh")
+else:
+ os.system("exit")
